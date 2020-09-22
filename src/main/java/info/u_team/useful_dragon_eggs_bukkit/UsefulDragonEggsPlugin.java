caf831623a679d72c683aa8589bc429b8c016acc
HyCraftHD <hycrafthd@live.de>
2020-09-22 16:33:02 +0200

    Register base listener

--- src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
@@ -2,6 +2,17 @@ package info.u_team.useful_dragon_eggs_bukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import info.u_team.useful_dragon_eggs_bukkit.event.UsefulDragonEggsFallHandler;
+
 public class UsefulDragonEggsPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new UsefulDragonEggsFallHandler(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+	}
 	
 }
---
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/event/UsefulDragonEggsFallHandler.java
@@ -0,0 +1,7 @@
+package info.u_team.useful_dragon_eggs_bukkit.event;
+
+import org.bukkit.event.Listener;
+
+public class UsefulDragonEggsFallHandler implements Listener {
+	
+}
