823c3f728e2fd6fa18aa714a94032b8a117bc462
HyCraftHD <hycrafthd@live.de>
2020-09-22 17:26:13 +0200

    Integrate config in the command now

--- src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
@@ -1,6 +1,5 @@
 package info.u_team.useful_dragon_eggs_bukkit;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import info.u_team.useful_dragon_eggs_bukkit.command.DragonEggsCommand;
@@ -10,8 +9,6 @@ public class UsefulDragonEggsPlugin extends JavaPlugin {
 	
 	private static UsefulDragonEggsPlugin instance;
 	
-	private FileConfiguration config;
-	
 	@Override
 	public void onLoad() {
 		instance = this;
@@ -19,9 +16,7 @@ public class UsefulDragonEggsPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		config = getConfig();
-		
-		config.addDefault("bedrockBreaking", true);
+		getConfig().addDefault("bedrockBreaking", true);
 		
 		getServer().getPluginManager().registerEvents(new UsefulDragonEggsFallHandler(), this);
 		getServer().getPluginCommand("dragoneggs").setExecutor(new DragonEggsCommand());
--- src/main/java/info/u_team/useful_dragon_eggs_bukkit/command/DragonEggsCommand.java
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/command/DragonEggsCommand.java
@@ -4,13 +4,16 @@ import java.util.*;
 
 import org.bukkit.command.*;
 
+import info.u_team.useful_dragon_eggs_bukkit.UsefulDragonEggsPlugin;
+
 public class DragonEggsCommand implements TabExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length >= 1 && args[0].equals("bedrock-breaking")) {
 			if (args.length == 1) {
-				sender.sendMessage("Value bedrock-breaking is currently set to: xyz");
+				final boolean bedrockBreaking = UsefulDragonEggsPlugin.getInstance().getConfig().getBoolean("bedrockBreaking");
+				sender.sendMessage("Value bedrock-breaking is currently set to: " + bedrockBreaking);
 				return true;
 			} else if (args.length == 2) {
 				final String argValue = args[1];
@@ -20,7 +23,10 @@ public class DragonEggsCommand implements TabExecutor {
 				
 				if (valueTrue || valueFalse) {
 					final boolean value = valueTrue ? true : false;
-					sender.sendMessage("Value bedrock-breaking is now set to: xyz");
+					UsefulDragonEggsPlugin.getInstance().getConfig().set("bedrockBreaking", value);
+					UsefulDragonEggsPlugin.getInstance().saveConfig();
+					final boolean bedrockBreaking = UsefulDragonEggsPlugin.getInstance().getConfig().getBoolean("bedrockBreaking");
+					sender.sendMessage("Value bedrock-breaking is now set to: " + bedrockBreaking);
 					return true;
 				}
 			}
