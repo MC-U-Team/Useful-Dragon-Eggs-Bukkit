8e91486717ee969fa1efa1a9080bb4c0d476db59
HyCraftHD <hycrafthd@live.de>
2020-09-22 17:23:21 +0200

    Add basic config

--- src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/UsefulDragonEggsPlugin.java
@@ -1,5 +1,6 @@
 package info.u_team.useful_dragon_eggs_bukkit;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import info.u_team.useful_dragon_eggs_bukkit.command.DragonEggsCommand;
@@ -7,14 +8,31 @@ import info.u_team.useful_dragon_eggs_bukkit.event.UsefulDragonEggsFallHandler;
 
 public class UsefulDragonEggsPlugin extends JavaPlugin {
 	
+	private static UsefulDragonEggsPlugin instance;
+	
+	private FileConfiguration config;
+	
+	@Override
+	public void onLoad() {
+		instance = this;
+	}
+	
 	@Override
 	public void onEnable() {
+		config = getConfig();
+		
+		config.addDefault("bedrockBreaking", true);
+		
 		getServer().getPluginManager().registerEvents(new UsefulDragonEggsFallHandler(), this);
 		getServer().getPluginCommand("dragoneggs").setExecutor(new DragonEggsCommand());
 	}
 	
 	@Override
 	public void onDisable() {
+	}
+	
+	public static UsefulDragonEggsPlugin getInstance() {
+		return instance;
 	}
 	
 }
