6bf5bfbac58b49d2d6b6883258db3f396774c355
HyCraftHD <hycrafthd@live.de>
2020-09-27 00:52:51 +0200

    Add craftworld class reference in nms util

--- src/main/java/info/u_team/useful_dragon_eggs_bukkit/nms/NmsUtil.java
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/nms/NmsUtil.java
@@ -1,9 +1,36 @@
 package info.u_team.useful_dragon_eggs_bukkit.nms;
 
-import org.bukkit.Bukkit;
+import org.bukkit.*;
 
 public class NmsUtil {
 	
 	public static final String NMS_VERSION = Bukkit.getServer().getClass().getPackage().getName().substring(23);
+	
+	private static final Class<?> CRAFT_WORLD_CLASS;
+	
+	static {
+		CRAFT_WORLD_CLASS = getNmsClass("org.bukkit.craftbukkit", "CraftWorld");
+	}
+	
+	public static void WorldServer$areChunksLoadedBetween(World world, Location location) {
+		
+		// Class.forName("org.bukkit.craftbukkit." + NMS_VERSION + ".CraftWorld").getMethod("getHandle");
+		
+		// final WorldServer world = ((CraftWorld) event.getEntity().getWorld()).getHandle();
+		
+	}
+	
+	private static Class<?> getNmsClass(String basePackage, String subPackage) {
+		try {
+			return Class.forName(getNmsPackage(basePackage, subPackage));
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static String getNmsPackage(String basePackage, String subPackage) {
+		return basePackage + "." + NMS_VERSION + "." + subPackage;
+	}
 	
 }
