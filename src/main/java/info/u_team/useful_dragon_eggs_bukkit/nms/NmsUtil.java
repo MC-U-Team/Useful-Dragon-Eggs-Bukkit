00fadd3ac604b1b29e21a0ae6524981f4a522190
HyCraftHD <hycrafthd@live.de>
2020-09-26 16:08:29 +0200

    Get nms util

---
+++ src/main/java/info/u_team/useful_dragon_eggs_bukkit/nms/NmsUtil.java
@@ -0,0 +1,9 @@
+package info.u_team.useful_dragon_eggs_bukkit.nms;
+
+import org.bukkit.Bukkit;
+
+public class NmsUtil {
+	
+	public static final String NMS_VERSION = Bukkit.getServer().getClass().getPackage().getName().substring(23);
+	
+}
